2ae57fd95fc1ae7a6d0b5c7deab613e60851feae
abhijit kumar singh <abhijitkumarsingh.cse58@gmail.com>
2025-06-25 14:46:14 +0530

    pricing aka benefits section modified

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -4,8 +4,18 @@ import Section from "./Section";
 import Heading from "./Heading";
 import { benefits } from "../constants";
 import Arrow from "../assets/svg/Arrow";
-import ClipPath from "../assets/svg/ClipPath";
+// import ClipPath from "../assets/svg/ClipPath";
 import { GradientLight } from "./design/Benefits";
+
+const ClipPath = () => {
+  return (
+    <svg className="block" width={0} height={0}>
+      <clipPath id="benefits" clipPathUnits="objectBoundingBox">
+        <path d="M0.079,0 h0.756 a0.079,0.083,0,0,1,0.058,0.026 l0.086,0.096 A0.079,0.083,0,0,1,1,0.179 V0.917 c0,0.046,-0.035,0.083,-0.079,0.083 H0.079 c-0.044,0,-0.079,-0.037,-0.079,-0.083 V0.083 C0,0.037,0.035,0,0.079,0" />
+      </clipPath>
+    </svg>
+  );
+};
 
 const Benefits = () => {
   return (
